d3bc53bf6b5d78560c1ce7afd24ff2797171f74f
Vladimir Rodríguez <fizzvr@gmail.com>
2022-05-12 18:49:00 -0500

    📦 NEW: componentes iniciales

---
+++ src/componentes/especialidades/index.js
@@ -0,0 +1,48 @@
+import React from 'react';
+import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
+
+const EspecialidadesComponentes = () => (
+  <>
+    <Typography variant='h4' component='div' gutterBottom>
+      Especialidades
+    </Typography>
+    <Typography variant='body1' component='div' gutterBottom>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Debitis
+      voluptatibus pariatur cum consequatur officia aut necessitatibus obcaecati
+      fuga. Ducimus voluptas reiciendis inventore deserunt. Necessitatibus unde
+      explicabo sunt veritatis officia commodi! Obcaecati, ut iste nesciunt
+      eveniet perspiciatis consectetur expedita accusamus ducimus inventore
+      consequatur pariatur laudantium optio eos, ab odit rerum. Nisi dignissimos
+      laudantium sit labore accusantium dolor expedita tenetur fuga molestias!
+      Perferendis ducimus rerum ea, eaque eos totam praesentium deserunt
+      voluptatibus ipsa explicabo perspiciatis corrupti dolorem quas minus
+      necessitatibus quam saepe animi corporis id. Iste, voluptatum voluptatibus
+      aperiam eius itaque hic. Iure neque nisi totam, ut adipisci dolore
+      excepturi eius dicta laudantium vitae rem accusantium est. Placeat saepe
+      itaque earum veniam repudiandae voluptatibus animi, neque quas cupiditate,
+      perferendis, mollitia reiciendis quasi! Nostrum nesciunt laboriosam atque
+      modi, fugit similique suscipit eveniet reiciendis fugiat mollitia minima
+      culpa odio esse magni quos qui? Suscipit, minima. Illum excepturi sit est
+      tenetur ad laudantium sequi soluta. Cupiditate voluptatum dicta est quasi
+      architecto nemo tenetur? Explicabo dolores eveniet, minima maiores debitis
+      provident! Recusandae sed reprehenderit ratione voluptatem molestiae velit
+      modi, omnis adipisci cum soluta hic voluptate nam! Atque accusamus quidem
+      velit, hic molestias eum, repellat vel totam quod ratione debitis earum
+      vitae necessitatibus, nobis fuga! Fuga ratione nam cumque corporis
+      excepturi quaerat laborum voluptas magni rem repudiandae! Recusandae est
+      quod, ipsam at aperiam libero dignissimos quia sit fuga temporibus facilis
+      odit esse neque ea earum minima excepturi enim cumque nam culpa inventore
+      labore soluta. A, consequatur voluptatem. Repudiandae consectetur iure
+      veritatis nulla et, tempore facere esse corrupti nisi doloribus neque
+      tenetur quidem iste odit ducimus ea sed aperiam? Praesentium assumenda
+      consectetur atque ullam beatae! Debitis, beatae amet! Natus aspernatur
+      totam iste nihil, ab odit omnis molestias inventore impedit quaerat? Eum
+      accusantium doloremque, natus dolore architecto sequi quis, odio optio
+      beatae, totam assumenda repellat porro animi unde accusamus.
+    </Typography>
+    <Button variant='contained'>Especialidades</Button>
+  </>
+);
+
+export default EspecialidadesComponentes;
