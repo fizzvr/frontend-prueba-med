8b41f42d2e8c018d847136fce1799498942026d7
Vladimir Rodríguez <fizzvr@gmail.com>
2022-05-12 22:05:46 -0500

    📦 NEW: se carga el fondo de pantalla desde la API de la nasa

--- src/App.js
+++ src/App.js
@@ -1,10 +1,51 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import BienvenidoPagina from './contenedores/BienvenidoPagina';
+import Box from '@mui/material/Box';
+import axios from 'axios';
+import CircularProgress from '@mui/material/CircularProgress';
+import Appbar from './componentes/appbar';
+import Backdrop from '@mui/material/Backdrop';
 
-const App = () => (
-  <>
-    <BienvenidoPagina />
-  </>
-);
+const App = () => {
+  const [data, setData] = useState();
+
+  useEffect(() => {
+    axios
+      .get(
+        `https://api.nasa.gov/planetary/apod?api_key=yyDaMAY7Y33pfIbFveHIBJdOpb02ygmukFSTTeYk`
+      )
+      .then((res) => {
+        setData(res.data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, []);
+
+  if (data === undefined) {
+    return (
+      <Backdrop
+        sx={{ color: '#fff', zIndex: (theme) => theme.zIndex.drawer + 1 }}
+        open={true}
+      >
+        <CircularProgress color='secondary' />
+      </Backdrop>
+    );
+  }
+  return (
+    <Box
+      sx={{
+        height: '100vh',
+        backgroundSize: 'cover',
+        backgroundRepeat: 'no-repeat',
+        backgroundPosition: 'center center',
+        backgroundImage: `url('${data.url}')`
+      }}
+    >
+      <Appbar />
+      <BienvenidoPagina />;
+    </Box>
+  );
+};
 
 export default App;
