04604eb1e36b48411ed3e6818f3f5818bf65e70e
Vladimir Rodríguez <fizzvr@gmail.com>
2022-05-12 22:06:50 -0500

    🐛 FIX: centrado de botones

--- src/contenedores/BienvenidoPagina.js
+++ src/contenedores/BienvenidoPagina.js
@@ -1,21 +1,30 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Box from '@mui/material/Box';
 import Container from '@mui/material/Container';
 import Grid from '@mui/material/Grid';
 import Medicos from '../componentes/medicos';
 import Especialidades from '../componentes/especialidades';
+import axios from 'axios';
 
-const BienvenidoPagina = () => (
-  <Container component='main' maxWidth='lg'>
-    <Grid container spacing={4}>
-      <Grid item key={1} md={6} sm={6} xs={12}>
-        <Medicos />
+const BienvenidoPagina = () => {
+  return (
+    <Container component='main' maxWidth='lg' sx={{ padding: '150px' }}>
+      <Grid
+        container
+        spacing={6}
+        direction='column'
+        alignItems='center'
+        justify='center'
+      >
+        <Grid item key={1} md={6} sm={6} xs={12}>
+          <Medicos />
+        </Grid>
+        <Grid item key={2} md={6} sm={6} xs={12}>
+          <Especialidades />
+        </Grid>
       </Grid>
-      <Grid item key={1} md={6} sm={6} xs={12}>
-        <Especialidades />
-      </Grid>
-    </Grid>
-  </Container>
-);
+    </Container>
+  );
+};
 
 export default BienvenidoPagina;
