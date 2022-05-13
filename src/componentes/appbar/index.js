a437b050ce5b824a2a7d9ef63f539f891d2fe3c8
Vladimir Rodríguez <fizzvr@gmail.com>
2022-05-13 13:06:49 -0500

    📦 NEW: Link a las rutas en el appbar

--- src/componentes/appbar/index.js
+++ src/componentes/appbar/index.js
@@ -4,20 +4,36 @@ import Box from '@mui/material/Box';
 import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
 import Button from '@mui/material/Button';
+import { Link } from 'react-router-dom';
+import { makeStyles } from '@material-ui/core/styles';
 
-export default function ButtonAppBar() {
+const useStyles = makeStyles((theme) => ({
+  aire: {
+    textDecoration: 'none',
+    '&:hover': {
+      textDecoration: 'none'
+    }
+  }
+}));
+
+export default function Superior() {
+  const classes = useStyles();
   return (
     <Box sx={{ flexGrow: 1 }}>
       <AppBar position='static'>
-        
         <Toolbar>
           <Typography variant='h6' component='div' sx={{ flexGrow: 1 }}>
-            Metrored - Prueba Técnica
+            <Link to='/' className={classes.aire}>
+              Metrored - Prueba Técnica
+            </Link>
           </Typography>
-          <Button color='inherit'>Medicos</Button>
-          <Button color='inherit'>Especialidades</Button>
+          <Button color='inherit' component={Link} to='/medicos'>
+            Medicos
+          </Button>
+          <Button color='inherit' component={Link} to='/especialidades'>
+            Especialidades
+          </Button>
         </Toolbar>
-        
       </AppBar>
     </Box>
   );
