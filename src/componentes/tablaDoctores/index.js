f1de83b32e174b9fc2a3eceb0ccb9004b266f6d0
Vladimir Rodríguez <fizzvr@gmail.com>
2022-05-14 11:17:03 -0500

    📦 NEW: se muestra la data relacional desde la API

--- src/componentes/tablaDoctores/index.js
+++ src/componentes/tablaDoctores/index.js
@@ -1,12 +1,33 @@
 import React, { useEffect, useState } from 'react';
 import { DataGrid } from '@mui/x-data-grid';
+import Backdrop from '@mui/material/Backdrop';
+import CircularProgress from '@mui/material/CircularProgress';
+import { getMedicos } from '../../servicios/medicos';
+
+const getEspecialidad = (params) => {
+  return `${params.value.nombreEspecialidad}`;
+};
+
+const getCiudad = (params) => {
+  return `${params.value.nombreCiudad}`;
+};
 
 const columns = [
   { field: 'id', headerName: 'ID', width: 10 },
   { field: 'nombre', headerName: 'NOMBRE', width: 250, sortable: false },
   { field: 'cedula', headerName: 'CEDULA', width: 150 },
-  { field: 'ciudad', headerName: 'CIUDAD', width: 150 },
-  { field: 'especialidad', headerName: 'ESPECIALIDAD', width: 150 },
+  {
+    field: 'cat_ciudades',
+    headerName: 'CIUDAD',
+    width: 150,
+    valueGetter: getCiudad
+  },
+  {
+    field: 'cat_especialidades',
+    headerName: 'ESPECIALIDAD',
+    width: 150,
+    valueGetter: getEspecialidad
+  },
   {
     field: 'foto',
     headerName: 'FOTO',
@@ -14,33 +35,36 @@ const columns = [
   }
 ];
 
-const rows = [
-  {
-    id: 1,
-    nombre: 'JHON DOE',
-    cedula: '1719067777',
-    ciudad: 'QUITO',
-    especialidad: 'PEDIATRÍA',
-    foto: 'https://source.unsplash.com/random'
-  },
-  {
-    id: 2,
-    nombre: 'LAURA DOE',
-    cedula: '1719067744',
-    ciudad: 'CUENCA',
-    especialidad: 'ALERGOLOGÍA',
-    foto: 'https://source.unsplash.com/random'
-  }
-];
-
-export default function DataTable() {
+export default function TablaDoctores() {
   const [data, setData] = useState();
   console.log('data', data);
+  const [rowData, setRowData] = useState({});
+  const [loaded, setLoaded] = useState(false);
+  console.log('rowData', rowData);
+
+  useEffect(() => {
+    getMedicos().then((items) => {
+      setRowData(items.nombre);
+      setLoaded(true);
+    });
+    return () => setLoaded(false);
+  }, []);
+
+  if (loaded === false) {
+    return (
+      <Backdrop
+        sx={{ color: '#fff', zIndex: (theme) => theme.zIndex.drawer + 1 }}
+        open={true}
+      >
+        <CircularProgress color='secondary' />
+      </Backdrop>
+    );
+  }
 
   return (
     <div style={{ height: 500, width: '100%' }}>
       <DataGrid
-        rows={rows}
+        rows={rowData}
         columns={columns}
         pageSize={5}
         rowsPerPageOptions={[5]}
