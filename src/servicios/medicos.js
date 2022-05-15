17b6b0ae7b49a1e4daddabfd86bc4d3250d48591
Vladimir Rodríguez <fizzvr@gmail.com>
2022-05-15 09:13:30 -0500

    📦 NEW: servicios CRUD medicos

--- src/servicios/medicos.js
+++ src/servicios/medicos.js
@@ -4,3 +4,45 @@ export function getMedicos() {
   var apiurl = 'http://localhost:5000/api/cat_medicos/asignado';
   return axios.get(apiurl).then((response) => response.data);
 }
+
+export function setMedicos(item) {
+  return axios
+    .post('http://localhost:5000/api/cat_medicos', item)
+    .then((reponse) => {
+      console.log('reponse', reponse);
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+}
+
+export function getMedicosID(item) {
+  return axios
+    .get(`http://localhost:5000/api/cat_medicos/${item}`)
+    .then((response) => response.data)
+    .catch((error) => {
+      console.log(error);
+    });
+}
+
+export function updateMedico(item) {
+  return axios
+    .put('http://localhost:5000/api/cat_medicos', item)
+    .then((reponse) => {
+      console.log('reponse', reponse);
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+}
+export function deleteMedico(item) {
+ 
+  return axios
+    .delete(`http://localhost:5000/api/cat_medicos/${item}`)
+    .then((reponse) => {
+      console.log('reponse', reponse);
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+}
