5315b71c7735e79c95ac091a30fbae48e07fcfb7
Vladimir Rodríguez <fizzvr@gmail.com>
2022-05-14 21:27:11 -0500

    📦 NEW: llamo al componente

--- src/contenedores/EspecialidadesPagina.js
+++ src/contenedores/EspecialidadesPagina.js
@@ -1,55 +1,13 @@
-import React, { useEffect, useState } from 'react';
-import TablaEspecialidades from '../componentes/tablaEspecialidades';
+import React from 'react';
 import Container from '@mui/material/Container';
-import Grid from '@mui/material/Grid';
-import Button from '@mui/material/Button';
-import { Link } from 'react-router-dom';
 import Paper from '@mui/material/Paper';
-import Typography from '@mui/material/Typography';
+import MainEspecialidades from '../componentes/especialidades';
 
 const EspecialidadesPagina = () => {
   return (
-    <Container component='main' maxWidth='lg' sx={{ paddingTop: '70px' }}>
+    <Container component='main' maxWidth='lg' sx={{ paddingTop: '50px' }}>
       <Paper sx={{ backgroundColor: '#ccc', padding: '15px' }}>
-        <Typography variant='h4' gutterBottom component='div'>
-          Lista Especialidades
-        </Typography>
-        <Grid container direction='row' alignItems='top' justify='center'>
-          <Grid item key={1} md={10} sm={6} xs={12}>
-            <TablaEspecialidades />
-          </Grid>
-          <Grid item key={2} md={2} sm={6} xs={12}>
-            <Button
-              variant='contained'
-              color='success'
-              component={Link}
-              to='especialidades'
-              sx={{ margin: '20px' }}
-            >
-              Nuevo
-            </Button>
-            <Button
-              disabled={true}
-              variant='contained'
-              color='info'
-              component={Link}
-              to='especialidades'
-              sx={{ margin: '20px' }}
-            >
-              Modificar
-            </Button>
-            <Button
-              disabled={true}
-              variant='contained'
-              color='error'
-              component={Link}
-              to='especialidades'
-              sx={{ margin: '20px' }}
-            >
-              Borrar
-            </Button>
-          </Grid>
-        </Grid>
+        <MainEspecialidades />
       </Paper>
     </Container>
   );
