7c8858f4b43b96a2af5799fa26058a73f50e49f7
Vladimir Rodríguez <fizzvr@gmail.com>
2022-05-14 21:26:48 -0500

    📦 NEW: ENDPOINTS especialidades

--- src/servicios/especialidades.js
+++ src/servicios/especialidades.js
@@ -4,3 +4,46 @@ export function getEspecialidades() {
   var apiurl = 'http://localhost:5000/api/cat_especialidades';
   return axios.get(apiurl).then((response) => response.data);
 }
+
+export function getEspecialidadID(item) {
+  return axios
+    .get(`http://localhost:5000/api/cat_especialidades/${item}`)
+    .then((response) => response.data)
+    .catch((error) => {
+      console.log(error);
+    });
+}
+
+export function setEspecialidad(item) {
+  return axios
+    .post('http://localhost:5000/api/cat_especialidades', item)
+    .then((reponse) => {
+      console.log('reponse', reponse);
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+}
+
+export function updateEspecialidad(item) {
+  return axios
+    .put('http://localhost:5000/api/cat_especialidades', item)
+    .then((reponse) => {
+      console.log('reponse', reponse);
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+}
+export function deleteEspecialidad(item) {
+  console.log('ASDASDASD', item)
+  
+  return axios
+    .delete(`http://localhost:5000/api/cat_especialidades/${item}`)
+    .then((reponse) => {
+      console.log('reponse', reponse);
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+}
